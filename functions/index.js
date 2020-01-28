2f8ee0dde69d623d3242bf55009957f9c044b110
natehubbit <natehubbit1001@gmail.com>
2020-01-28 15:19:40 +0000

    InitCommit

---
+++ functions/index.js
@@ -0,0 +1,45 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp()
+
+
+exports.onUSerMessageCreated = functions.database.ref('/Messages/{messageId}/{senderId}/{dataId}')
+.onCreate(async (snapshot,context)=>{
+
+    const { message,from,to } = snapshot._data
+
+    if(from !== context.params.senderId) return
+
+    console.log('Message Created')
+
+    // GET SENDER DATA
+    const senderPromise = admin.database().ref('/Users/'+from).once('value')
+    // GET RECEIVER NOTIFICATION TOKEN
+    const deviceTokenPromise = admin.database().ref(`/Tokens/${to}`).once('value')
+
+    // RESOLVE PROMISES
+    const results = await Promise.all([senderPromise, deviceTokenPromise])
+    const sender = results[0].val()
+    const notificationToken = results[1].val()
+
+    const payload = {
+        notification:{
+            title: sender.name,
+            body:message,
+            icon:sender.image,
+        }
+    }
+
+    // const tokensToRemove = []
+
+    const response = await admin.messaging().sendToDevice(notificationToken.token,payload)
+    const {error} = response.results[0];
+    if (error) {
+        console.error('Failure sending notification to', notificationToken.token, error);
+        // Cleanup the tokens who are not registered anymore.
+        if (error.code === 'messaging/invalid-registration-token' ||
+            error.code === 'messaging/registration-token-not-registered') {
+        // tokensToRemove.push(no.ref.child(tokens[index]).remove());
+        }
+    }
+})
